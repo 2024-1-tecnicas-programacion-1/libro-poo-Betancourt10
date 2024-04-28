b3da5b181bc02aee46e2be9ae58816277aaeb23a
Betancourt10 <Felibetan2005@gmail.com>
2024-04-27 22:40:53 -0500

    Taller 2

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,42 @@
 package com.mycompany.libropoo;
 
-public class Libro {
+public class Libro { 
+    //O(1)
+    private String titulo; 
+    private String autor; 
+    private int anoPublicacion; 
+    private String genero;
+    private boolean leido; 
+    //O(1)
+    public Libro(String titulo, String autor, int anoPublicacion, boolean leido) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacion = anoPublicacion; 
+        this.genero = genero;
+        this.leido = false;
+    }
+
+    Libro() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //O(1)
+    public String mostrarInformacion (){ 
+        return "Título: " + titulo + "Autor: " + autor + "Año de publicación: " + anoPublicacion + "Género: " + genero;
+    } 
+    //O(1)
+    public void marcarLeido() { 
+        leido = true; 
+    } 
+    //O(1) 
+    public boolean isLeido() { 
+        return leido;
+    }
+    //O(1)
+    public boolean esAntiguo() { 
+        if (anoPublicacion < 1974) 
+                return true;  
+        else  
+                return false;
+    }
     
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,33 @@
 package com.mycompany.libropoo;
+import java.util.Scanner;
 
 public class Principal {
-    
+
+    //O(1)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Buen dia.");
+        System.out.println("Por favor ingrese el titulo del libro:");
+        String titulo = sc.nextLine();
+        System.out.println("Ingrese el autor del libro:");
+        String autor = sc.nextLine();
+        System.out.println("Ingrese el género literario del libro:");
+        String genero = sc.nextLine();
+        System.out.println("Ingrese el año de publicación del libro");
+        int anoPublicacion = sc.nextInt();
+
+        boolean leido = false;
+        //O(1)
+        libroUsuario.marcarLeido();
+        System.out.println(libroUsuario.mostrarInformacion());
+        libroUsuario.isLeido();
+        if (libroUsuario.isLeido() == true) {
+            System.out.println("El libro " + titulo + " ya fue leído.");
+        } else {
+            System.out.println("No has leido " + titulo);
+        }
+        if (libroUsuario.esAntiguo() == true) {
+            System.out.println("El libro es antiguo.");
+        }
+    }
 }
